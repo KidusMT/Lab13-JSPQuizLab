8dccbfd3e1b2f0b93fcbf24d1e08dc51f9421c85
KidusMT <kidusmamuye@gmail.com>
2021-05-07 17:32:02 -0500

    done with Lab 12 Question for quiz

--- src/main/java/miu/kmt/Quiz.java
+++ src/main/java/miu/kmt/Quiz.java
@@ -5,12 +5,41 @@ package miu.kmt;
  * Date: 5/7/2021
  */
 public class Quiz {
-    private static String[] questions = {
+    private static int score = 0;
+
+    private static final String[] questions = {
             "3, 1, 4, 1, 5",   // pi
             "1, 1, 2, 3, 5",   // fibonacci
             "1, 4, 9, 16, 25", // squares
             "2, 3, 5, 7, 11",  // primes
             "1, 2, 4, 8, 16"   // powers of 2
     };
-    private static int[] answers = {9, 8, 26, 13, 32};
+    private static final int[] answers = {9, 8, 26, 13, 32};
+
+    public static boolean checkAnswer(int ans, int quesIndx) {
+        if (ans == answers[quesIndx]) {
+            if (score >= questions.length) score = 1;
+            else score++;
+            return true;
+        }
+        return false;
+    }
+
+    public static void setScore(int newScore){
+        score = newScore;
+    }
+
+    public static int getScore() {
+        return score;
+    }
+
+    public static String[] getAllQuestions() {
+        return questions;
+    }
+
+    public static String getQuestion(int index) {
+        return questions[index];
+    }
+
+
 }
--- src/main/java/miu/kmt/QuizServlet.java
+++ src/main/java/miu/kmt/QuizServlet.java
@@ -1,20 +1,87 @@
 package miu.kmt;
 
-import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author KidusMT
  * Date: 5/7/2021
  */
+@WebServlet({"/", "/answer"})
 public class QuizServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
-//        Cookie[] cookie = req.getCookies();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Cookie[] cookies = req.getCookies();
+
+        String answer = req.getParameter("ans");
+        Cookie current_question = new Cookie("current_question", "0");
+        current_question.setMaxAge(15 * 60 * 24);// 15 min
+        resp.addCookie(current_question);
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang='en'>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>Quiz App</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<div style='align-content: center'>");
+        out.println("<h1>The Number Quiz</h1>");
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("current_question".equals(cookie.getName())) {
+                    // current question number
+                    int curQst = Integer.parseInt(cookie.getValue());
+                    if (curQst == 0) {// if first question
+                        out.println("<p>Your current score is " + Quiz.getScore() + ".</p>" +
+                                "<p>Guess the next number in the sequence.</p>" +
+                                "<form action='/Lab12_ServletQuizLab_war_exploded/answer' method='get'>" +
+                                "<div style='display: inline-flex; flex-direction: column'>" +
+                                "<p>" + Quiz.getQuestion(0) + "</p>" +
+                                "<label for='answer'>Your answer: <input id='answer' name='ans' width='250px' type='text'/></label><br>" +
+                                "<input style='padding: 10px; margin: 20px 0; width: 100px' type='submit'>" + "</div>" +
+                                "</form>"
+                        );
+                        current_question.setValue(String.valueOf(curQst+1));
+                    } else {
+                        if (curQst < Quiz.getAllQuestions().length) { // if not last question
+                            int nextQuestion = curQst + 1;
+                            if (answer != null && !answer.isEmpty()) {
+                                Quiz.checkAnswer(Integer.parseInt(answer), curQst-1);
+                            }
+                            out.println("<p>Your current score is " + Quiz.getScore() + ".</p>" +
+                                    "<p>Guess the next number in the sequence.</p>" +
+                                    "<form action='/Lab12_ServletQuizLab_war_exploded/answer' method='get'>" +
+                                    "<div style='display: inline-flex; flex-direction: column'>" +
+                                    "<p>" + Quiz.getQuestion(curQst) + "</p>" +
+                                    "<label for='answer'>Your answer: <input id='answer' name='ans' width='250px' type='text'/></label><br>" +
+                                    "<input style='padding: 10px; margin: 20px 0; width: 100px' type='submit'>" + "</div>" +
+                                    "</form>"
+                            );
+                            current_question.setValue(String.valueOf(nextQuestion));
+                        } else {
+                            out.println("<p>Your current score is " + Quiz.getScore() + ".</p>" +
+                                    "<p>You have completed the Number Quiz, with a score of " + Quiz.getScore() +
+                                    " out of " + curQst + ".</p>");
+                            current_question.setValue("0");
+                            Quiz.setScore(0);
+                        }
+                    }
+                    resp.addCookie(current_question);
+                }
+            }
+        }
+
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }

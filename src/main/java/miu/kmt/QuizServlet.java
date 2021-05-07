7feb7a084a526e455bee91c3cfb3b70798bccdde
KidusMT <kidusmamuye@gmail.com>
2021-05-07 03:26:41 -0500

    created quiz servlet

---
+++ src/main/java/miu/kmt/QuizServlet.java
@@ -0,0 +1,20 @@
+package miu.kmt;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author KidusMT
+ * Date: 5/7/2021
+ */
+public class QuizServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+//        Cookie[] cookie = req.getCookies();
+    }
+}

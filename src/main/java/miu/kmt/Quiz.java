6f682ff35a09a29c4e85592540ea8ea37673242b
KidusMT <kidusmamuye@gmail.com>
2021-05-07 03:11:50 -0500

    added quiz model with questions and answer to the questions

---
+++ src/main/java/miu/kmt/Quiz.java
@@ -0,0 +1,16 @@
+package miu.kmt;
+
+/**
+ * @author KidusMT
+ * Date: 5/7/2021
+ */
+public class Quiz {
+    private static String[] questions = {
+            "3, 1, 4, 1, 5",   // pi
+            "1, 1, 2, 3, 5",   // fibonacci
+            "1, 4, 9, 16, 25", // squares
+            "2, 3, 5, 7, 11",  // primes
+            "1, 2, 4, 8, 16"   // powers of 2
+    };
+    private static int[] answers = {9, 8, 26, 13, 32};
+}
